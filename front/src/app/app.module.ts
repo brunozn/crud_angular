35ccbb002534f1c6738d573dee7c371efac1277d
brunojnd <brunojndias@gmail.com>
2021-01-19 18:07:23 -0300

    componente createComponet

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -16,6 +16,7 @@ import { MatListModule } from '@angular/material/list';
 import { HomeComponent } from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 import { RedDirective } from './directives/red.directive';
+import { ProductCreateComponent } from './components/product/product-create/product-create.component';
 
 
 
@@ -28,6 +29,7 @@ import { RedDirective } from './directives/red.directive';
     HomeComponent,
     ProductCrudComponent,
     RedDirective,
+    ProductCreateComponent,
   ],
   imports: [
     BrowserModule,
